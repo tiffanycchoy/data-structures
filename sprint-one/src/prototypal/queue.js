28198f6f722540650bf75c0358ef81210d677306
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-02 15:06:48 -0700

    fix tab formatting

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,6 +3,16 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function() {
 
+  }
 
+  dequeue: function() {
+
+  }
+
+  size: function() {
+    
+  }
+};
