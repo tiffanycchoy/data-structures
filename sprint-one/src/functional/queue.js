e986e9eb6e442d833b2968738e93273866252815
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-02 15:04:02 -0700

    fix tab formatting

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,29 +1,27 @@
 var Queue = function() {
-	var someInstance = {};
-	var storage = {};
-	var firstIndex = 0;
-	var lastIndex = 0;
+  var queueInstance = {};
+  var storage = {};
+  var firstIndex = 0;
+  var lastIndex = 0;
 
-	someInstance.enqueue = function(value) {
-		storage[lastIndex] = value;
-		lastIndex++
-	};
+  queueInstance.enqueue = function(value) {
+    storage[lastIndex] = value;
+    lastIndex++;
+  };
 
-	someInstance.dequeue = function() {
+  queueInstance.dequeue = function() {
     var dequeueValue;
-		if (lastIndex !== firstIndex) {
-			dequeueValue = storage[firstIndex]
-			delete storage[firstIndex];
-			firstIndex += 1;
-			return dequeueValue;
-		}
-	};
+    if (lastIndex !== firstIndex) {
+      dequeueValue = storage[firstIndex];
+      delete storage[firstIndex];
+      firstIndex++;
+      return dequeueValue;
+    }
+  };
 
-	someInstance.size = function() {
-		return lastIndex - firstIndex;
-	};
+  queueInstance.size = function() {
+    return lastIndex - firstIndex;
+  };
 
-	return someInstance;
-};
-
-module.exports = Queue;
+  return queueInstance;
+}
