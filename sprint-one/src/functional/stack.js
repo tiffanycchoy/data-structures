6d0d0bbeaa477760f8b46876c73affe3b9b19f5a
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-02 14:10:31 -0700

    implement stack using functional class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,27 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var size = 0;
   someInstance.push = function(value) {
+    storage[size] = value;
+    size += 1;
   };
 
   someInstance.pop = function() {
+    var poppedValue;
+    if(size > 0) {
+      poppedValue = storage[size - 1];
+      delete storage[size - 1];
+      size -= 1;
+      return poppedValue;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+module.exports = Stack;
