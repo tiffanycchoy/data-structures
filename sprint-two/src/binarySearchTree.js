2ac4efaa5eba88955f7122eda775fa57d137f6de
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-03 14:29:33 -0700

    implement binary search tree data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,54 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    //do nothing. value is already in the tree.
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if(this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right) {
+    this.right.depthFirstLog(cb);
+  }
+}
 
 
 /*
