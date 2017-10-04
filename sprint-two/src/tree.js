a5183d52076ede5c37a7ab787e8e3764233f9118
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-03 20:17:24 -0700

    add .removeFromParent() method to tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,12 @@
 var Tree = function(value) {
   this.value = value;
+  this.parent = null;
   this.children = [];
 };
 
 Tree.prototype.addChild = function(value) {
   var myTree = new Tree(value);
+  myTree.parent = this;
   this.children.push(myTree);
 };
 
@@ -26,7 +28,28 @@ Tree.prototype.contains = function(target) {
   return false;
 };
 
+Tree.prototype.removeFromParent = function() {
+  var parentTree = this.parent;
 
+  //remove child from parent's children.
+  for (var i = 0; i < parentTree.children.length; i++) {
+    if (parentTree.children[i].value === this.value) {
+      parentTree.children.splice(i, 1);
+    }
+  }
+
+  this.parent = null;
+}
+
+Tree.prototype.traverse = function(cb) {
+  cb(this.value);
+
+  for (var i = 0; i < this.children.length; i++) {
+    this.children[i].traverse(cb);
+  }
+}
+
+module.exports = Tree;
 
 /*
  * Complexity: What is the time complexity of the above functions?
